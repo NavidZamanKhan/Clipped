bb6c7255e05f1b31e7a4cf9c5ffcb52826541c92
Navid Zaman Khan <navid166418@gmail.com>
2026-07-12 01:59:05 +0600

    Initial Commit

---
+++ Clipped/ClippedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClippedApp.swift
+//  Clipped
+//
+//  Created by Navid Zaman Khan on 7/12/26.
+//
+
+import SwiftUI
+
+@main
+struct ClippedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
